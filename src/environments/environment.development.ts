ee5111fef10f4c147b4c42cb20329291a37c56eb
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-09-24 16:00:15 -0500

    Added Environment
    feat(environment): added conection with the server.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  // Server Base Path for API Nido Urbano
+  serverBasePath: 'http://localhost:3000/api/v1'
+  // serverBasePath for Sprint Boot REST API
+  //serverBasePath: 'http://localhost:8090/api/v1'
+};

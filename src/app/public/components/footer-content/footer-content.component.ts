f825c387671a6dd04274692bfd56a0b6291b700f
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-09-24 17:15:36 -0500

    Added Public
    feat(public): added of footer Copyright and the developer team.

--- src/app/public/components/footer-content/footer-content.component.ts
+++ src/app/public/components/footer-content/footer-content.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { TranslateModule } from "@ngx-translate/core";
+
 @Component({
   selector: 'app-footer-content',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer-content.component.html',
   styleUrl: './footer-content.component.css'
 })

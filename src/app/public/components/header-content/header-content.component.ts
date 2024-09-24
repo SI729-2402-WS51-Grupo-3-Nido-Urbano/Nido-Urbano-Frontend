d9889ad81c3f1a3d1075666a20e0f8fffd6de072
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-09-24 17:17:16 -0500

    Added Public
    feat(public): added header with button menu and toolbar.

--- src/app/public/components/header-content/header-content.component.ts
+++ src/app/public/components/header-content/header-content.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LanguageSwitcherComponent } from "../language-switcher/language-switcher.component";
 
 @Component({
   selector: 'app-header-content',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, LanguageSwitcherComponent],
   templateUrl: './header-content.component.html',
   styleUrl: './header-content.component.css'
 })

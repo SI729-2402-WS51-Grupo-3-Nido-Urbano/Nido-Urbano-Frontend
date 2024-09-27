3e7ead88194b99514eeb41771a783c4c20a1e2e4
Miguel Angel <u202220294@upc.edu.pe>
2024-09-26 23:40:28 -0500

    feat: home update - comprar/alquilar

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,28 +1,71 @@
 import { Component } from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {HouseCardComponent} from "../../components/house-card/house-card.component";
 import { MatTabsModule } from '@angular/material/tabs';
+import {House} from "../../../payments/model/house.entity";
+import {HousesService} from "../../../payments/services/houses.service";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NgForOf, HouseCardComponent, MatTabsModule
+    NgForOf, HouseCardComponent, MatTabsModule, NgIf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  houses = [
-    { price: '$300 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
-    { price: '$250 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
-    { price: '$400 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
-    { price: '$55 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
-    { price: '$250 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
-    { price: '$270 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
-    { price: '$320 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
-    { price: '$650 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' }
+  houseData: House;
+  housesSale: House[] = [];
+  housesRent: House[] = [];
+
+  // Constructor
+  constructor(private houseService: HousesService) {
+    this.houseData = {} as House;
+  }
+
+  ngOnInit() {
+    this.loadHousesSale(); // Cambia a 'alquiler' si deseas cargar casas de alquiler
+    this.loadHousesRent();
+  }
+
+  loadHousesSale() {
+    this.houseService.getHousesByType('comprar').subscribe(
+      (data) => {
+        this.housesSale = data;
+        console.log(this.housesSale);
+      },
+      (error) => {
+        console.error('Error fetching houses', error);
+      }
+    );}
+
+  loadHousesRent() {
+    this.houseService.getHousesByType('alquilar').subscribe(
+      (data) => {
+        this.housesRent = data;
+        console.log(this.housesRent);
+      },
+      (error) => {
+        console.error('Error fetching houses', error);
+      }
+    );}
+
+  /*housesSale = [
+    {ownerId: 1, amount: 30000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"},
+    {ownerId: 2, amount: 10000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"},
+    {ownerId: 3, amount: 20000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"},
+    {ownerId: 3, amount: 20000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"},
+    {ownerId: 3, amount: 20000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"},
+    {ownerId: 3, amount: 20000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"}
   ];
 
-
+  housesRent = [
+    {ownerId: 1, amount: 30000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"},
+    {ownerId: 2, amount: 10000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"},
+    {ownerId: 3, amount: 20000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"},
+    {ownerId: 3, amount: 20000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"},
+    {ownerId: 3, amount: 20000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"},
+    {ownerId: 3, amount: 20000, size: "5", spaces: 5, nearest: "Muy cerca", type: "compra", imageUrl: "https://i.postimg.cc/3NyhYt06/image.png", description: "Muy bonita"}
+  ]*/
 }

000b49add45e1ee58bd513e06930bcc325750841
Miguel Angel <u202220294@upc.edu.pe>
2024-09-26 23:41:27 -0500

    feat: house card semi-developed

--- src/app/public/components/house-card/house-card.component.ts
+++ src/app/public/components/house-card/house-card.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import {NgIf} from "@angular/common"; // Para los botones
+import {NgIf} from "@angular/common";
+import {House} from "../../../payments/model/house.entity";
+import {HousesService} from "../../../payments/services/houses.service"; // Para los botones
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-house-card',
@@ -12,5 +15,12 @@ import {NgIf} from "@angular/common"; // Para los botones
   styleUrl: './house-card.component.css'
 })
 export class HouseCardComponent {
+  @Input() house!: House;
 
+  constructor(private houseService: HousesService, private router: Router) {}
+
+  onBuy(): void {
+    this.houseService.setHouse(this.house);
+    this.router.navigate(['/payments', this.house.ownerId]); // Navegar a la ruta de pagos con el ID de la casa
+  }
 }

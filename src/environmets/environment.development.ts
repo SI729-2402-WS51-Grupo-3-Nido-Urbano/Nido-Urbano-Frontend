6483380be57fd930f38dcbd3d642b2520e037e5f
Miguel Angel <u202220294@upc.edu.pe>
2024-09-27 04:11:50 -0500

    feat: first develop version

--- src/environmets/environment.development.ts
+++ src/environmets/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
 // Server Base Path for Fake REST API
-  serverBasePath: 'http://localhost:3000/api/v1'
+  serverBasePath: 'https://my-json-server.typicode.com/SI729-2402-WS51-Grupo-3-Nido-Urbano/db-payments.json/api/v1'
 // Server Base Path for Spring Boot REST API
 //serverBasePath: 'http://localhost:8090/api/v1'
 };
--- src/environmets/environment.ts
+++ src/environmets/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverBasePath: 'http://localhost:3000/api/v1'
+  serverBasePath: 'https://my-json-server.typicode.com/SI729-2402-WS51-Grupo-3-Nido-Urbano/db-payments.json/api/v1'
 };

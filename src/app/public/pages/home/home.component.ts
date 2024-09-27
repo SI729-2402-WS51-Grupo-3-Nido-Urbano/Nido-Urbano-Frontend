ea91da19f91dd419ef624f01c3b72519e6284765
Miguel Angel <u202220294@upc.edu.pe>
2024-09-26 21:39:58 -0500

    feat: home component update

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {HouseCardComponent} from "../../components/house-card/house-card.component";
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf, HouseCardComponent, MatTabsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  houses = [
+    { price: '$300 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
+    { price: '$250 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
+    { price: '$400 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
+    { price: '$55 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
+    { price: '$250 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
+    { price: '$270 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
+    { price: '$320 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' },
+    { price: '$650 000 Dolares', image: 'https://i.postimg.cc/3NyhYt06/image.png' }
+  ];
+
 
 }

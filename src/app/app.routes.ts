7b46b575b6af53e2f383af69fb7bc78015e3ebeb
Miguel Angel <u202220294@upc.edu.pe>
2024-09-27 00:49:16 -0500

    feat: new routes confirmatio-bcp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from "./public/pages/home/home.component";
 import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
 import {PaymentManagmentComponent} from "./payments/pages/payment-managment/payment-managment.component";
+import {ConfirmationBCPComponent} from "./payments/pages/confirmation-bcp/confirmation-bcp.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'payments/:id', component: PaymentManagmentComponent },
+  { path: 'confirm-payment/:id', component: ConfirmationBCPComponent },
   { path: '**', component: PageNotFoundComponent }
 ];

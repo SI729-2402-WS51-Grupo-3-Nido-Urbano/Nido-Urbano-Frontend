8bb9d61c928e244c4948068c333ccfec08ee499a
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-10-14 16:16:46 -0500

    Created House Edit and Deleted
    feat(house): add component edit and deleted house.

---
+++ src/app/house-management/houses/components/houses-edit-delet/houses-edit-delet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-houses-edit-delet',
+  standalone: true,
+  imports: [],
+  templateUrl: './houses-edit-delet.component.html',
+  styleUrl: './houses-edit-delet.component.css'
+})
+export class HousesEditDeletComponent {
+
+}

3da492e07460d35723cc58c19c025c4145303970
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-10-14 00:40:21 -0500

    Add App Component
    feat(app-component): add app component for sidenav.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,60 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderContentComponent } from './public/components/header-content/header-content.component';
 import { FooterContentComponent } from './public/components/footer-content/footer-content.component';
-//import { ProductListComponent } from "./products/components/product-list/product-list.component";
 import { TranslateService } from "@ngx-translate/core";
+import { MatSidenav, MatSidenavContainer, MatSidenavModule } from "@angular/material/sidenav";
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatListItem, MatNavList } from "@angular/material/list";
+import { MatIcon } from "@angular/material/icon";
+import { LanguageSwitcherComponent } from "./public/components/language-switcher/language-switcher.component";
+import { MatToolbar } from "@angular/material/toolbar";
+import { HousesManagementsComponent } from "./house-management/houses/pages/houses-managements/houses-managements.component";
+import {HousesAddComponent} from "./house-management/houses/components/houses-add/houses-add.component";
+import {HousesAddsComponent} from "./house-management/houses/components/houses-adds/houses-adds.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderContentComponent, FooterContentComponent],
+  imports: [
+    RouterOutlet, HeaderContentComponent,
+    FooterContentComponent, MatListItem,
+    MatIcon, MatNavList,
+    MatSidenav, MatSidenavContainer,
+    LanguageSwitcherComponent, RouterLink,
+    MatToolbar, MatSidenavModule,
+    HousesManagementsComponent, HousesAddComponent, HousesAddsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'nido-urbano-frontend';
-  constructor(translate: TranslateService) {
+
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  options = [
+    { icon: 'home', path: '/home', title: 'Home' },
+    { icon: 'house', path: '/houses', title: 'Houses' },
+    { icon: 'info', path: '/about', title: 'About' }
+  ];
+
+  constructor(private translate: TranslateService, private observer: BreakpointObserver) {
     translate.setDefaultLang('en');
     translate.use('en');
+  }
+
+  ngAfterViewInit(): void {
+    this.observer.observe(['(max-width: 1280px)'])
+      .subscribe((response) => {
+        if (this.sidenav) {
+          if (response.matches) {
+            this.sidenav.mode = 'over';
+            this.sidenav.close();
+          } else {
+            this.sidenav.mode = 'side';
+            this.sidenav.open();
+          }
+        }
+      });
   }
 }

20f84f561e589222074995388a586cc2cf8ce9d0
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-10-13 23:02:12 -0500

    Add Verification Entity
    feat(verification-entity): add the used verification structure.

---
+++ src/app/house-management/verification/model/verification.entity.ts
@@ -0,0 +1,12 @@
+export class Verification {
+    id: number;
+    verification_type: string;
+    verification_date: string;
+    verification_status: string;
+    constructor() {
+        this.id = 0;
+        this.verification_type = '';
+        this.verification_date = '';
+        this.verification_status = '';
+    }
+}

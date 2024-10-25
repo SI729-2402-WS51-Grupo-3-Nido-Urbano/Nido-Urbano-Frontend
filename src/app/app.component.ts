0368355ddcca4999168c99f356940a4d06fe4e59
Aranda Vallejos, Oscar Gabriel <134239824+OscarGAV@users.noreply.github.com>
2024-10-24 21:50:28 -0500

    feat: Calendar With View of Dates
    Added a view of the dates available in a calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import { OnInit, ViewChild } from '@angular/core';
-
-import { RouterLink } from '@angular/router';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule } from "@angular/material/divider";
 import { MatListModule } from "@angular/material/list";
-
 import { BreakpointObserver } from "@angular/cdk/layout";
-
 import { TranslateService } from "@ngx-translate/core";
 import { LanguageSwitcherComponent } from "./public/components/language-switcher/language-switcher.component";
-
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatIconModule,
-    MatSidenavModule, MatDividerModule, MatListModule, LanguageSwitcherComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatListModule,
+    LanguageSwitcherComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']  // Corregido: styleUrls en lugar de styleUrl
 })
-export class AppComponent implements OnInit  {
+export class AppComponent implements OnInit {
   title = 'housing-reservation';
 
-  @ViewChild(MatSidenav, {static: true}) sidenav!: MatSidenav;
+  @ViewChild(MatSidenav, { static: true }) sidenav!: MatSidenav;
+
   options = [
-    { icon: 'explore', path: '/housing-reservation/reservations', title: 'Appointments'},
-    { icon: 'event', path: '/housing-reservation/calendar', title: 'Dates'},
+    { icon: 'explore', path: '/housing-reservation/reservations', title: 'Appointments' },
+    { icon: 'event', path: '/housing-reservation/calendar', title: 'Dates' }
   ];
 
   constructor(private translate: TranslateService, private observer: BreakpointObserver) {
@@ -42,15 +43,14 @@ export class AppComponent implements OnInit  {
   }
 
   ngOnInit(): void {
-    this.observer.observe(['(max-width: 1280px)']) // Observa el ancho de la pantalla
-      .subscribe((response) => {  // Se suscribe a los cambios en el ancho de la pantalla
-        if (response.matches) { // Si el ancho de la pantalla es menor a 1280px
-          this.sidenav.mode = 'over'; // Se despliega sobre el contenido
-          this.sidenav.close(); // Se cierra
-        } else {
-          this.sidenav.mode = 'side'; // Se despliega al lado del contenido
-          this.sidenav.open();  // Se abre
-        }
-      });
+    this.observer.observe(['(min-width: 280px)']).subscribe(response => {
+      if (response.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
   }
 }
--- src/app/housing-reservation/pages/reservation-calendar-management/calendar-management.component.ts
+++ src/app/housing-reservation/pages/reservation-calendar-management/calendar-management.component.ts
@@ -1,164 +1,33 @@
-/**
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {provideNativeDateAdapter} from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {DatePipe, NgIf} from "@angular/common";
-import {ReservationCalendarErrorDialog} from "../../components/reservation-calendar-error-dialog/reservation-calendar-error-dialog";
-import {MatDialog} from "@angular/material/dialog";
-import {Reservation} from "../../model/reservation.entity";
-import {ReservationManagementService} from "../../services/reservation-management.service";
-import {Router} from '@angular/router';
-
-
-@Component({
-  selector: 'datepicker-inline-calendar-example',
-  templateUrl: 'calendar-management.component.html',
-  styleUrl: 'calendar-management.component.css',
-  standalone: true,
-  providers: [provideNativeDateAdapter()],
-  imports: [MatCardModule, MatDatepickerModule, DatePipe, NgIf],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class CalendarManagementComponent {
-  selectedDate: Date | null = null; // Fecha actualmente seleccionada
-  startDate: Date | null = null; // Fecha de inicio
-  endDate: Date | null = null; // Fecha de fin
-  currentDate: Date = new Date(); // Fecha actual
-  reservations: Reservation[] = [];
-  reservedDates: { start: Date; end: Date }[] = []; // Store reserved date ranges
-  nextId: number = 1; // Next reservation ID
-
-  constructor(private dialog: MatDialog,
-              private reservationManagementService: ReservationManagementService,
-              private router: Router) {}
-
-  ngOnInit() {
-    this.getNextReservationId(); // Fetch the next available ID on initialization
-  }
-
-  getNextReservationId(): void {
-    this.reservationManagementService.getNextId().subscribe(
-      (nextId) => {
-        this.nextId = nextId; // Set the next available ID
-      },
-      (error) => {
-        console.error('Error fetching next reservation ID:', error);
-      }
-    );
-  }
-
-  onDateChange(date: Date | null) {
-    if (date && date < this.currentDate) {
-      // Si la fecha seleccionada es inferior a la fecha actual
-      this.openErrorDialog(); // Abre el diálogo de error
-      return;
-    }
-
-    if (!this.startDate) {
-      // Si no hay fecha de inicio, se establece como la fecha seleccionada
-      this.startDate = date;
-    } else if (!this.endDate && date && date > this.startDate) {
-      // Si ya hay una fecha de inicio y no hay fecha de fin, se establece la fecha seleccionada como fin
-      this.endDate = date;
-    } else {
-      // Si ya hay una fecha de inicio y fin, reiniciar la selección
-      this.startDate = date;
-      this.endDate = null; // Reiniciar fecha de fin
-    }
-    this.selectedDate = date; // Actualiza la fecha seleccionada para el calendario
-  }
-
-  openErrorDialog(): void {
-    this.dialog.open(ReservationCalendarErrorDialog, {
-      width: '300px',
-    });
-  }
-
-  onContinue() {
-    if (!this.startDate || !this.endDate) {
-      this.openErrorDialog(); // Abre el diálogo de error
-      return;
-    }
-
-    // Calculate the difference in days between startDate and endDate
-    const diffInTime = this.endDate.getTime() - this.startDate.getTime();
-    const diffInDays = diffInTime / (1000 * 3600 * 24); // Convert milliseconds to days
-
-    // Check if the difference exceeds 2 days
-    if (diffInDays > 2) {
-      this.openErrorDialog(); // Open the error dialog if the difference is more than 2 days
-      return;
-    }
-
-    if (this.startDate && this.endDate) {
-      console.log(`Start Date: ${this.startDate}, End Date: ${this.endDate}`);
-
-      // Crear la cita con las fechas seleccionadas
-      const newReservation: Reservation = {
-        id: this.nextId, // Use the calculated next ID
-        tenant_name: "Nombre del Inquilino",
-        age: 30, // Valor de ejemplo
-        tenant_address: "Dirección del Inquilino",
-        house_name: "Nombre de la Casa",
-        house_address: "Dirección de la Casa",
-        landlord_name: "Nombre del Propietario",
-        start_date: this.startDate, // Asigna la fecha de inicio seleccionada
-        end_date: this.endDate // Asigna la fecha de fin seleccionada
-      };
-
-      // Llama al servicio para crear la reserva
-      this.reservationManagementService.createReservation(newReservation)
-        .subscribe(response => {
-          console.log('Reserva creada exitosamente:', response);
-          // Resetea las fechas de inicio y fin
-          this.startDate = null;
-          this.endDate = null;
-          // Redirige a la ruta deseada
-          this.router.navigate(['housing-reservation/reservations']);
-        }, error => {
-          console.error('Error al crear la reserva:', error);
-          this.openErrorDialog(); // Abre el diálogo en caso de error
-        });
-    }
-  }
-}*/
-
-import {ChangeDetectionStrategy, Component, ViewEncapsulation, OnInit} from '@angular/core';
-import {provideNativeDateAdapter} from '@angular/material/core';
-import {MatCalendarCellClassFunction, MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, OnInit, ChangeDetectorRef } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatCalendarCellClassFunction, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 import { ReservationManagementService } from "../../services/reservation-management.service";
 import { Reservation } from '../../model/reservation.entity';
 import { CommonModule } from '@angular/common';
-
 import { TranslateModule } from "@ngx-translate/core";
 
-
-/** @title Datepicker with custom date classes */
 @Component({
   selector: 'datepicker-inline-calendar-example',
   templateUrl: 'calendar-management.component.html',
-  styleUrl: 'calendar-management.component.css',
+  styleUrls: ['calendar-management.component.css'],
   encapsulation: ViewEncapsulation.None,
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatCardModule, TranslateModule],
+  imports: [MatFormFieldModule, MatDatepickerModule, MatCardModule, TranslateModule, CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class CalendarManagementComponent implements OnInit {
-  reservedDates: { start: Date, end: Date }[] = []; // Guardará las fechas de las reservas
+  reservedDates: { start: Date, end: Date }[] = [];
 
-  constructor(private reservationManagementService: ReservationManagementService) {}
+  constructor(
+    private reservationManagementService: ReservationManagementService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    // Cargar las reservas cuando se inicializa el componente
     this.reservationManagementService.getAllReservations().subscribe((reservations: Reservation[]) => {
-      // Mapeamos las fechas de inicio y fin de cada reserva
       this.reservedDates = reservations.map(reservation => ({
         start: new Date(reservation.start_date),
         end: new Date(reservation.end_date)
@@ -166,33 +35,31 @@ export class CalendarManagementComponent implements OnInit {
 
       // Ajustar horas a principio y final del día
       this.reservedDates = this.reservedDates.map(range => ({
-        start: new Date(range.start.setHours(0, 0, 0, 0)), // Establecer hora a 00:00:00
-        end: new Date(range.end.setHours(23, 59, 59, 999)) // Establecer hora a 23:59:59.999
+        start: new Date(range.start.setHours(0, 0, 0, 0)), // Inicio del día
+        end: new Date(range.end.setHours(23, 59, 59, 999)) // Fin del día
       }));
 
       console.log(this.reservedDates); // Log para verificar
+
+      // Usar setTimeout para forzar la actualización del calendario
+      setTimeout(() => {
+        this.cdr.detectChanges(); // Forzar detección de cambios
+      }, 0);
     });
   }
 
-
-
-  // Función que aplica la clase CSS a las fechas del calendario
   dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
     if (view === 'month') {
-      return this.isDateInRange(cellDate) ? 'unavailable-dates' : 'available-dates';
+      return this.isDateInRange(cellDate) ? 'unavailable-dates' : '';
     }
     return '';
   };
 
-  // Función para verificar si la fecha está en algún rango de reserva
   isDateInRange(date: Date): boolean {
     return this.reservedDates.some(range => {
-      // Ajustar la fecha de fin a las 23:59:59.999 para incluir el último día completo
       const endDateAdjusted = new Date(range.end);
-      endDateAdjusted.setHours(23, 59, 59, 999); // Establecer a 23:59:59.999
-
-      console.log(`Evaluando: ${date} entre ${range.start} y ${endDateAdjusted}`); // Log para verificar evaluaciones
-      return date >= range.start && date <= endDateAdjusted; // Incluir el último día en la comparación
+      endDateAdjusted.setHours(23, 59, 59, 999); // Fin del día
+      return date >= range.start && date <= endDateAdjusted;
     });
   }
 }

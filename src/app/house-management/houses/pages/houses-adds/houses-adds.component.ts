4bbd0af2db6da8575a5dfda6edc4ee01a2c35f97
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-10-28 21:41:36 -0500

    fix(houses-adds):
    fixed the code and improved the code.

--- src/app/house-management/houses/pages/houses-adds/houses-adds.component.ts
+++ src/app/house-management/houses/pages/houses-adds/houses-adds.component.ts
@@ -38,89 +38,45 @@ interface Status{
   styleUrl: './houses-adds.component.css'
 })
 export class HousesAddsComponent {
-  private _formBuilder = inject(FormBuilder);
+  private fb = inject(FormBuilder);
   private housesService = inject(HousesService);
-  houseTypeControl = new FormControl<Type | null>(null, Validators.required);
-  houseModalControl = new FormControl<Modal | null>(null, Validators.required);
-  startsCalificationControl = new FormControl<Starts | null>(null, Validators.required);
-  statusLandlordControl = new FormControl<Status | null>(null, Validators.required);
+
+  houseForm: FormGroup = this.fb.group({
+    userPropertyIdCtrl: ['', Validators.required],
+    houseNameCtrl: ['', Validators.required],
+    houseAddressCtrl: ['', Validators.required],
+    houseTypeControl: ['', Validators.required],
+    houseModalControl: ['', Validators.required],
+    housePriceCtrl: ['', Validators.required],
+    houseSizeCtrl: ['', Validators.required],
+    houseDescriptionCtrl: ['', Validators.required],
+    startsCalificationControl: ['', Validators.required],
+    statusLandlordControl: ['', Validators.required],
+    houseImageCtrl: ['', [Validators.required, Validators.pattern('https?://.+')]],
+    houseVideoCtrl: ['', [Validators.required, Validators.pattern('https?://.+')]],
+    verificationIdCtrl: ['', Validators.required],
+  });
 
   houseTyp: Type[] = [
-    {type: 'HOUSE'},
-    {type: 'APARTMENT'},
+    { type: 'HOUSE' },
+    { type: 'APARTMENT' },
   ];
   houseMod: Modal[] = [
-    {modal: 'RENTAL'},
-    {modal: 'SALE'},
+    { modal: 'RENTAL' },
+    { modal: 'SALE' },
   ];
   startCalification: Starts[] = [
-    {starts: 1},
-    {starts: 2},
-    {starts: 3},
-    {starts: 4},
-    {starts: 5},
+    { starts: 1 },
+    { starts: 2 },
+    { starts: 3 },
+    { starts: 4 },
+    { starts: 5 },
   ];
   statusLand: Status[] = [
-    {status: 'NO REQUEST'},
-    {status: 'IN PROGRESS'},
-    {status: 'LEASED'},
+    { status: 'NO REQUEST' },
+    { status: 'IN PROGRESS' },
+    { status: 'LEASED' },
   ];
-
-  userPropertyId = this._formBuilder.group({
-    userPropertyIdCtrl: ['', Validators.required]
-  });
-  houseName = this._formBuilder.group({
-    houseNameCtrl: ['', Validators.required]
-  });
-  houseAddress = this._formBuilder.group({
-    houseAddressCtrl: ['', Validators.required]
-  });
-  houseType = this._formBuilder.group({
-    houseTypeControl: ['', Validators.required]
-  });
-  houseModal = this._formBuilder.group({
-    houseModalControl: ['', Validators.required]
-  });
-  housePrice = this._formBuilder.group({
-    housePriceCtrl: ['', Validators.required]
-  });
-  houseSize = this._formBuilder.group({
-    houseSizeCtrl: ['', Validators.required]
-  });
-  houseDescription = this._formBuilder.group({
-    houseDescriptionCtrl: ['', Validators.required]
-  });
-  startsCalification = this._formBuilder.group({
-    startsCalificationControl: ['', Validators.required]
-  });
-  statusLandlord = this._formBuilder.group({
-    statusLandlordControl: ['', Validators.required]
-  });
-  houseImage = this._formBuilder.group({
-    houseImageCtrl: ['', [Validators.required, Validators.pattern('https?://.+')]]
-  });
-  houseVideo = this._formBuilder.group({
-    houseVideoCtrl: ['', [Validators.required, Validators.pattern('https?://.+')]]
-  });
-  verificationId = this._formBuilder.group({
-    verificationIdCtrl: ['', Validators.required]
-  });
-  houseForm: FormGroup = this._formBuilder.group({
-    userPropertyIdCtrl: this.userPropertyId.get('userPropertyIdCtrl'),
-    houseNameCtrl: this.houseName.get('houseNameCtrl'),
-    houseAddressCtrl: this.houseAddress.get('houseAddressCtrl'),
-    houseTypeControl: this.houseType.get('houseTypeControl'),
-    houseModalControl: this.houseModal.get('houseModalControl'),
-    housePriceCtrl: this.housePrice.get('housePriceCtrl'),
-    houseSizeCtrl: this.houseSize.get('houseSizeCtrl'),
-    houseDescriptionCtrl: this.houseDescription.get('houseDescriptionCtrl'),
-    startsCalificationControl: this.startsCalification.get('startsCalificationControl'),
-    statusLandlordControl: this.statusLandlord.get('statusLandlordControl'),
-    houseImageCtrl: this.houseImage.get('houseImageCtrl'),
-    houseVideoCtrl: this.houseVideo.get('houseVideoCtrl'),
-    verificationIdCtrl: this.verificationId.get('verificationIdCtrl')
-  });
-
 
   onSubmit() {
     if (this.houseForm.valid) {
@@ -138,24 +94,23 @@ export class HousesAddsComponent {
         status_landlord: this.houseForm.value.statusLandlordControl,
         photo: this.houseForm.value.houseImageCtrl,
         video: this.houseForm.value.houseVideoCtrl,
-        verification_id: this.houseForm.value.verificationIdCtrl
+        verification_id: this.houseForm.value.verificationIdCtrl,
       };
 
       console.log(houseData);
 
-      this.housesService.create(houseData)
-        .subscribe(
-          response => {
-            console.log("Datos enviados correctamente:", response);
-          },
-          error => {
-            console.error("Error al enviar los datos:", error);
-          }
-        );
+      this.housesService.create(houseData).subscribe(
+        (response) => {
+          console.log('Datos enviados correctamente:', response);
+        },
+        (error) => {
+          console.error('Error al enviar los datos:', error);
+        }
+      );
     } else {
       console.log('Formulario incompleto o contiene errores');
     }
-
   }
-  isLinear = false;
+
+  isLinear = true;
 }

81ee598f9ec213301cb8f62ab13da5fd5f140a0e
Aranda Vallejos, Oscar Gabriel <134239824+OscarGAV@users.noreply.github.com>
2024-10-28 21:05:14 -0500

    feat: Add Contracts Bounded Context

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from "./public/pages/home/home.component";
 import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
-import {PaymentManagmentComponent} from "./payments/pages/payment-managment/payment-managment.component";
-import {ConfirmationBCPComponent} from "./payments/pages/confirmation-bcp/confirmation-bcp.component";
+import { PaymentManagmentComponent } from "./payments/pages/payment-managment/payment-managment.component";
+import { ConfirmationBCPComponent } from "./payments/pages/confirmation-bcp/confirmation-bcp.component";
+import { ContractFormPageComponent} from "./contracts/pages/contract-form-page/contract-form-page.component";
+
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'payments/:id', component: PaymentManagmentComponent },
   { path: 'confirm-payment/:id', component: ConfirmationBCPComponent },
+  { path: 'generate', component: ContractFormPageComponent },
+
 
   { path: '**', component: PageNotFoundComponent }
 ];
---
+++ src/app/contracts/services/contract.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Contract, Landlord, Property, Tenant} from "../model/contract.entity";
+//import {environment} from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContractService {
+  private apiUrl = `https://my-json-server.typicode.com/AlexlmL/dbserver/contracts`;
+  private propertyUrl = `https://my-json-server.typicode.com/AlexlmL/dbserver/properties`;
+  private tenantUrl = `https://my-json-server.typicode.com/AlexlmL/dbserver/tenants`;
+  private landlordUrl = `https://my-json-server.typicode.com/AlexlmL/dbserver/landlords`;
+
+  constructor(private http: HttpClient) {}
+  getProperties() {
+    return this.http.get<Property[]>(this.propertyUrl); // Método para obtener propiedades
+  }
+  getContracts() {
+    return this.http.get<Contract[]>(this.apiUrl);
+  }
+  getTenants() {
+    return this.http.get<Tenant[]>(this.tenantUrl); // Método para obtener inquilinos
+  }
+  getLandlords() {
+    return this.http.get<Landlord[]>(this.landlordUrl);  // URL para obtener los landlords
+  }
+
+  createContract(contractData: Contract) {
+    return this.http.post(this.apiUrl, contractData);
+  }
+
+  updateContract(id: number, contractData: Contract) {
+    return this.http.put(`${this.apiUrl}/${id}`, contractData);
+  }
+
+  cancelContract(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}

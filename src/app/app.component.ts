12113b5ed87efe800c8649188efe7c768689d294
Aranda Vallejos, Oscar Gabriel <134239824+OscarGAV@users.noreply.github.com>
2024-10-28 19:54:21 -0500

    Revert "Merge pull request #1 from SI729-2402-WS51-Grupo-3-Nido-Urbano/feat/contracts"
    This reverts commit 484764a8991c8596cb7e56df810d23a5a27d9e8b, reversing
    changes made to 3515f27ec86bc45b0aa73fdb3e061a56d2c3f916.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Router } from '@angular/router';
-import { RouterLink } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatDividerModule } from "@angular/material/divider";
-import { MatListModule } from "@angular/material/list";
-import {ContractFormComponent} from "./contracts/components/contract-form/contract-form.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatIconModule,
-    MatSidenavModule, MatDividerModule, MatListModule, ContractFormComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'nido-urbano-frontend';
-
-  constructor(private router: Router) {}
-
-  navigateToContractForm() {
-    this.router.navigate(['/generate-contract']); // Asegúrate de que la ruta esté bien configurada
-  }
 }
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  serverBasePath: 'https://my-json-server.typicode.com/AlexlmL/dbserver'
-};
-

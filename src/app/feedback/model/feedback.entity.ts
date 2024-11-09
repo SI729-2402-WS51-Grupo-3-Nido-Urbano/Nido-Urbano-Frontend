3b36482f57ea4ec25560ab253c5225e94fa135fa
Rodrigo Chirinos <u202217804@upc.edu.pe>
2024-11-09 17:00:16 -0500

    Update feedback.entity.ts

--- src/app/feedback/model/feedback.entity.ts
+++ src/app/feedback/model/feedback.entity.ts
@@ -1,17 +1,16 @@
 export class Feedback {
   id: number;
+  property_id: number;
+  user_id: number;
   user_name: string;
-  rated_user_id: number;
-  rating_user_id: number;
   score: number;
   comments: string;
   rating_date: Date;
 
   constructor() {
     this.id = 0;
-    this.user_name = "";
-    this.rated_user_id = 0;
-    this.rating_user_id = 0;
+    this.property_id = 0;
+    this.user_id = 0;
     this.score = 0;
     this.comments = "";
     this.rating_date = new Date();

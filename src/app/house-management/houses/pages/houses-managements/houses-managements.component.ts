76e37bf8a9bf9a87a7ae7a558dc2f84e993c3aff
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-21 14:46:55 -0500

    feat(house-manangment): add activateTab.

--- src/app/house-management/houses/pages/houses-managements/houses-managements.component.ts
+++ src/app/house-management/houses/pages/houses-managements/houses-managements.component.ts
@@ -17,40 +17,47 @@ import {CommonModule} from "@angular/common";
   templateUrl: './houses-managements.component.html',
   styleUrl: './houses-managements.component.css'
 })
-export class HousesManagementsComponent implements OnInit{
+export class HousesManagementsComponent implements OnInit {
 
   houseData: House;
-  houseSale: House[]=[];
-  houseRent: House[]=[];
+  houseSale: House[] = [];
+  houseRent: House[] = [];
+  activeTab: string = 'SALE';
 
   constructor(private houseService: HousesService) {
     this.houseData = {} as House;
   }
+
   ngOnInit() {
-    this.loadHouseSale();
-    this.loadHouseRent();
+    this.loadHouses();
   }
 
-  loadHouseSale(){
-    this.houseService.getHouseByHouseModal('SALE').subscribe((data)=>{
-      this.houseSale = data;
-      console.log(this.houseSale);
-    },
-      (error)=>{
-      console.error('Error fetching house',error);
+  loadHouses() {
+    if (this.activeTab === 'SALE') {
+      this.houseService.getHouseByHouseModal('SALE').subscribe(
+        (data) => {
+          this.houseSale = data;
+          console.log(this.houseSale);
+        },
+        (error) => {
+          console.error('Error fetching houses for sale', error);
+        }
+      );
+    } else if (this.activeTab === 'RENTAL') {
+      this.houseService.getHouseByHouseModal('RENTAL').subscribe(
+        (data) => {
+          this.houseRent = data;
+          console.log(this.houseRent);
+        },
+        (error) => {
+          console.error('Error fetching houses for rent', error);
+        }
+      );
     }
-    );
-  }
-  loadHouseRent(){
-    this.houseService.getHouseByHouseModal('RENTAL').subscribe((data)=>{
-        this.houseRent = data;
-        console.log(this.houseRent);
-      },
-      (error)=>{
-        console.error('Error fetching house',error);
-      }
-    );
   }
 
-
+  onTabChange(index: number) {
+    this.activeTab = index === 0 ? 'SALE' : 'RENTAL';
+    this.loadHouses();
+  }
 }
